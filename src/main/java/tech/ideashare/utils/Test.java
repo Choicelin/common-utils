0a65bb73180ffa0039866b29cb55bbbc4aebee2c
lixiang <lixiang9409@vip.qq.com>
2018-03-14 23:54:36 +0800

    换成spring2.0

---
+++ src/main/java/tech/ideashare/utils/Test.java
@@ -0,0 +1,14 @@
+package tech.ideashare.utils;
+
+/**
+ * @Author lixiang
+ * @CreateTime 07/03/2018
+ **/
+public class Test {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
